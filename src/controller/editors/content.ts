84ef34300b2e97cfdf2fa9420dae07eb0334179f
Torsten Knauf <Torsten.Knauf@maibornwolff.de>
2019-12-17 16:43:04 +0100

    add empty lines in merge result for "padding" / aligning

--- src/controller/editors/content.ts
+++ src/controller/editors/content.ts
@@ -5,9 +5,17 @@ import { VersionProvider } from '../../virtual-documents/version-provider';
 export const getLocalChanges = (conflict: IConflict): string =>
   getTextContent(conflict.mergeResult, conflict.localChanges);
 
-export const getMergeResult = (conflict: IConflict): string => {
-  return conflict.mergeResult.join('\n');
-};
+export const getMergeResult = (conflict: IConflict): string =>
+  conflict.localChanges
+    .slice()
+    .reverse()
+    .reduce((acc, localConflict) => {
+      const startPadding = localConflict.startLineAdded + 1;
+      const paddingSize = localConflict.addedLines.length;
+      acc.splice(startPadding, 0, ...Array(paddingSize).fill(''));
+      return acc;
+    }, conflict.mergeResult.slice())
+    .join('\n');
 
 export const getRemoteChanges = (conflict: IConflict): string => {
   return 'todo: static content so far';
