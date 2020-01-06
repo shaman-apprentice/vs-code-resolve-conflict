1050a7bf75517efa3b2ff28ccdbe95bd579764cc
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-01-06 15:30:29 +0100

    update check for resolved conflicts

--- src/commands/apply-resolve-conflict.ts
+++ src/commands/apply-resolve-conflict.ts
@@ -3,13 +3,15 @@ import * as vscode from 'vscode';
 import { StateManager } from '../controller/state-manager';
 
 export const applyResolveConflict = (ctx: any) => {
-  vscode.window.showErrorMessage('Some conflicts are still on fire');
-  // todo probably something like:
-  // const conflicts = [
-  //   ...StateManager.parsedConflict.localChanges,
-  //   ...StateManager.parsedConflict.remoteChanges,
-  // ].filter(c => c.wasAdded);
-  // conflicts.length
-  //   ? StateManager.save()
-  //   : vscode.window.showErrorMessage('Some conflicts are still on fire');
+  const nonResolvedConflicts = [
+    ...StateManager.data.localChanges.conflicts,
+    ...StateManager.data.remoteChanges.conflicts,
+  ].filter(c => !c.isResolved);
+
+  if (nonResolvedConflicts.length) {
+    vscode.window.showErrorMessage('Some conflicts are still on fire');
+    return;
+  }
+
+  return StateManager.save();
 };
